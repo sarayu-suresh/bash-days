3d413eed2cc61edccad66365f619e8226f28afa7
sarayu-suresh <sarayusuresh888@gmail.com>
2021-05-31 19:37:00 +0530

    new

---
+++ echo-example.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+#when echo is used without any options a new line is added by default
+echo "Printing text in new line"
+# ‘-n’ option is used to print any text without new line and ‘-e’ option is used to remove backslash characters from the output. 
+echo -n "Printing text without creating new line"
+echo -e "\nRemoving the bakslash \tcharacters\n"
